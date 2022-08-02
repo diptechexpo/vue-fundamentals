9577f3e06b4ba1c79e9e4f841df322ddd4b60e54
Dipankar <dipankar.saha@rbc.com>
2022-08-01 21:57:44 -0500

    Added mounting and axios call

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -26,40 +26,44 @@
 
 <script>
 import PersonItem from "./PersonItem";
+import axios from "axios";
+
+const friends = [];
 
 export default {
   data() {
     return {
-      friends: [
-        {
-          id: 1,
-          firstName: "Michelle",
-          lastName: "Mulroy",
-          gender: "female",
-          fav: false
-        },
-        {
-          id: 2,
-          firstName: "Venkat",
-          lastName: "Subramanian",
-          gender: "male",
-          fav: false
-        },
-        {
-          id: 3,
-          firstName: "Matt",
-          lastName: "Forsythe",
-          gender: "none",
-          fav: false
-        },
-        {
-          id: 4,
-          firstName: "Nate",
-          lastName: "Schutta",
-          gender: "male",
-          fav: false
-        }
-      ]
+      friends
+      // friends: [
+      //   {
+      //     id: 1,
+      //     firstName: "Michelle",
+      //     lastName: "Mulroy",
+      //     gender: "female",
+      //     fav: false
+      //   },
+      //   {
+      //     id: 2,
+      //     firstName: "Venkat",
+      //     lastName: "Subramanian",
+      //     gender: "male",
+      //     fav: false
+      //   },
+      //   {
+      //     id: 3,
+      //     firstName: "Matt",
+      //     lastName: "Forsythe",
+      //     gender: "none",
+      //     fav: false
+      //   },
+      //   {
+      //     id: 4,
+      //     firstName: "Nate",
+      //     lastName: "Schutta",
+      //     gender: "male",
+      //     fav: false
+      //   }
+      // ]
     };
   },
   methods: {
@@ -69,9 +73,21 @@ export default {
     like(frnd) {
       console.log(frnd);
       frnd.fav = !frnd.fav;
+
+      // axios.patch(`http://localhost:3000/friends/${frnd.id}`, {
+      //   fav: frnd.fav
+      // })
+
+      axios.put(`http://localhost:3000/friends/${frnd.id}`, frnd);
+
     }
   },
-  components: { PersonItem }
+  components: { PersonItem },
+  mounted() {
+    axios.get("http://localhost:3000/friends").then(response => {
+      this.friends = response.data;
+    });
+  }
 };
 </script>
 
